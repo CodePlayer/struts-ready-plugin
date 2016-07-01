db56007aef55eb5467bd5fb753084d3535a156bd
Ready <CodePlayer360@gmail.com>
2016-07-01 11:53:15 +0800

    升级依赖组件版本

--- struts-ready-plugin/src/main/java/me/codeplayer/struts2/ReadyInterceptor.java
+++ struts-ready-plugin/src/main/java/me/codeplayer/struts2/ReadyInterceptor.java
@@ -4,13 +4,13 @@ import java.lang.reflect.Method;
 
 import javax.servlet.http.HttpServletResponse;
 
-import me.codeplayer.annotation.Ready;
-
 import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.ActionProxy;
 import com.opensymphony.xwork2.interceptor.Interceptor;
+
+import me.codeplayer.annotation.Ready;
 
 /**
  * struts-ready-plugin的核心拦截器，action的对应方法必须具有@Ready注解
@@ -22,9 +22,11 @@ public class ReadyInterceptor implements Interceptor {
 
 	private static final long serialVersionUID = 1L;
 
-	public void destroy() {}
+	public void destroy() {
+	}
 
-	public void init() {}
+	public void init() {
+	}
 
 	public String intercept(ActionInvocation invocation) throws Exception {
 		ActionProxy proxy = invocation.getProxy();
